7c6512055a102f5ba4d029e32184b113faa68148
Alban Tiberghien <alban.tiberghien@gmail.com>
2015-08-28 13:54:26 +0200

    Initial commit

---
+++ static/js/agency.js
@@ -0,0 +1,42 @@
+/*!
+ * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top}, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    var map = L.map('map').setView([44.5244, 3.4782], 12);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+           attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    var marker = L.marker([44.524956, 3.479604]).addTo(map);
+    marker.bindPopup("<b>Códi n camp<br/>Avenue du 11 novembre<br/>48000 Mende<br />").openPopup();
+    // map.dragging.disable();
+    // map.touchZoom.disable();
+    map.doubleClickZoom.disable();
+    map.scrollWheelZoom.disable();
+    map.boxZoom.disable();
+    map.keyboard.disable();
+    if (map.tap) map.tap.disable();
+    document.getElementById('map').style.cursor='default';
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
