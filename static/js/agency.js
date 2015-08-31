7ece01ba5220092f82af7fc68ff6e3d8f97ead3c
Alban Tiberghien <alban.tiberghien@gmail.com>
2015-08-31 08:30:06 +0200

    Update website

--- static/js/agency.js
+++ static/js/agency.js
@@ -12,7 +12,7 @@ $(function() {
         event.preventDefault();
     });
 
-    var map = L.map('map').setView([44.5244, 3.4782], 12);
+    var map = L.map('map').setView([44.5244, 3.4782], 13);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
